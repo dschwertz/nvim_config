b604ddad865c6808b0fee76cf3dd6b9faae84c9c
Dillon Schwertz <dschwertz800@gmail.com>
2024-06-10 09:20:20 +0600

    Adds trouble severity swap function

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,20 +1,21 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-    opts = {
-      fast_wrap = {
-        map = '<leader>fw',
-        chars = { '{', '[', '(', '"', "'" },
-        pattern = [=[[%'%"%>%]%)%}%,]]=],
-        end_key = '$',
-        before_key = 'h',
-        after_key = 'l',
-        cursor_pos_before = true,
-        keys = 'qwertyuiopzxcvbnmasdfghjkl',
-        manual_position = true,
-        highlight = 'Search',
-        highlight_grey='Comment'
-      }
-    } -- this is equalent to setup({}) function
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  config = true,
+  lazy = false,
+  opts = {
+    fast_wrap = {
+      map = '<leader>fw',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = [=[[%'%"%>%]%)%}%,]]=],
+      end_key = '$',
+      before_key = 'h',
+      after_key = 'l',
+      cursor_pos_before = true,
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      manual_position = true,
+      highlight = 'Search',
+      highlight_grey = 'Comment'
+    }
+  } -- this is equalent to setup({}) function
 }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,16 @@
 local opts = {
-  severity = "ERROR"
+  s = { -- example of a custom action that toggles the severity
+    action = function(view)
+      local f = view:get_filter("severity")
+      local severity = ((f and f.filter.severity or 0) + 1) % 5
+      view:filter({ severity = severity }, {
+        id = "severity",
+        template = "{hl:Title}Filter:{hl} {severity}",
+        del = severity == 0,
+      })
+    end,
+    desc = "Toggle Severity Filter",
+  },
 }
 
 return {
